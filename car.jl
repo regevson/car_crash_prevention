be57c18decec8181c27c0ef805c83cc7e845771f
regevson <juwal.regev@hotmail.com>
2020-08-14 17:05:50 +0200

    Implement CTRV state model - implemented prediction according to CTRV model - two cars now drive simultaneously

--- car.jl
+++ car.jl
@@ -4,6 +4,11 @@ using DataStructures
 #Vector = Array{Float64, 1}
 Float_Tuple = Tuple{Float64, Float64}
 Queue_Tuple = Queue{NTuple{6, Float64}}
+ω = 0.1 # yaw_rate
+τ = 0.1 # time_step
+
+# θ -> theta = angle
+# ω -> omega = yaw_rate
 
 mutable struct Car
 
@@ -13,6 +18,35 @@ mutable struct Car
 end
 
 function get_drive_path(car::Car)
+	if(isempty(car.drive_path))
+	   return
+   end
 	return dequeue!(car.drive_path)
 end
+
+function yaw_rate(deg)
+	return deg*1.1
+end
+
+function predict_trajectory(car::Car, sec_ahead)
+
+	new_q = Queue{NTuple{6, Float64}}()
+	enqueue!(new_q, first(car.drive_path))
+
+	for i = 0:τ:sec_ahead
+		(p_x, p_y, tmp1, tmp2, θ, vel) = last(new_q)
+		p_x_n = p_x + vel/ω * (sin(θ + ω*τ) - sin(θ))
+		p_y_n = p_y + vel/ω * (cos(ω) - cos(ω+θ*τ))
+		θ_n= ω*τ+θ
+		enqueue!(new_q, (p_x_n, p_y_n, tmp1, tmp2, θ_n, vel))
+		println("PREDICTION: ", p_x_n, " (p_x_n), ", p_y_n, " (p_y_n), ", θ_n, " (new_angle), ", vel, " vel ") 
+	end
+
+	car.drive_path = new_q	
+
+end
+
+
+
+
 
--- street.jl
+++ street.jl
@@ -7,10 +7,10 @@ include("car.jl")
 time = 0
 
 # building street
-street_km = 0:100
+street_km = 0:500
 lineA(x) = 0
-lineB(x) = 10
-lineC(x) = 20
+lineB(x) = 250
+lineC(x) = 500
 
 scene = Scene(resolution = (1500, 500))
 
@@ -18,9 +18,15 @@ lines!(scene, street_km, lineA, color = :green, linewidth = 4)
 lines!(scene, street_km, lineB, color = :red, linewidth = 4)
 lines!(scene, street_km, lineC, color = :green, linewidth = 4)
 
-function calc_normal_drive(q)
-	for i = 1:0.1:100
-		enqueue!(q, (25.0+i, 5.0, 10.0, 0.0, 0.0, 50.0))
+function calc_normal_drive(p_x, p_y, a_x, a_y, θ, v, q)
+	x_add = v/10
+	for i = 1:0.1:8
+		yaw_angle = 0.0
+		if i >= 5
+			yaw_angle = 0.5
+		end
+		enqueue!(q, (p_x+x_add, p_y, a_x, a_y, θ, v))
+		x_add = x_add + v/10
 	end
 end
 
@@ -30,34 +36,45 @@ function paint_car(obs_car)
 	obs2 = lift(x -> [x], obs_car[2])
 	obs3 = lift(x -> [x], obs_car[3])
 	obs4 = lift(x -> [x], obs_car[4])
-	scene = arrows!(obs1, obs2, obs3, obs4, linewidth = 64, arrowsize = 7)
+	scene = arrows!(obs1, obs2, obs3, obs4, linewidth = 44, arrowsize = 60)
 
 end
 
 function drive_car(car::Car, obs_car)
 
-	for i = 1:0.1:120
+	for i = 1:0.1:10
+		if i == 5 
+			predict_trajectory(car, 3)
+		end
 		position_vector = get_drive_path(car)
+		println(position_vector)
 		obs_car[1][] = position_vector[1]
 		obs_car[2][] = position_vector[2]
 		obs_car[3][] = position_vector[3]
 		obs_car[4][] = position_vector[4]
-		sleep(0.01)
+		sleep(0.1)
 	end
 
 end
 
 # build car_1
 q_drivepath_c1 = Queue{NTuple{6, Float64}}()
-calc_normal_drive(q_drivepath_c1)
-obs_car1 = [Node(25.0), Node(5.0), Node(10.0), Node(0.0)]
+calc_normal_drive(25.0, 100.0, 30.0, 0.0, 0.5, 50, q_drivepath_c1)
+obs_car1 = [Node(25.0), Node(100.0), Node(30.0), Node(0.0)]
 car_1 = Car(q_drivepath_c1)
 paint_car(obs_car1)
 
+# build car_2
+q_drivepath_c2 = Queue{NTuple{6, Float64}}()
+calc_normal_drive(25.0, 400.0, 30.0, 0.0, 0.001, 30, q_drivepath_c2)
+obs_car2 = [Node(500.0), Node(400.0), Node(-30.0), Node(0.0)]
+car_2 = Car(q_drivepath_c2)
+paint_car(obs_car2)
 
 display(scene)
-#@spawn drive_car()
-drive_car(car_1, obs_car1)
+@spawn drive_car(car_1, obs_car1)
+#drive_car(car_1, obs_car1)
+drive_car(car_2, obs_car2)
 
 
 
