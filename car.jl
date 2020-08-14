a72304eee3bbbb1579359d8ad8178f2f5a038b57
regevson <juwal.regev@hotmail.com>
2020-08-14 23:39:55 +0200

    Add danger-detection - car gets notified when other car is going to crash   into it (prediction)

--- car.jl
+++ car.jl
@@ -1,9 +1,14 @@
-using Makie
+module CarModule
+
+import Base.Threads.@spawn
 using DataStructures
 
-#Vector = Array{Float64, 1}
+export Float_Tuple, State_Vec, Queue_State_Vec, Car, get_drive_path, predict_trajectory, observe_new_car
+
 Float_Tuple = Tuple{Float64, Float64}
-Queue_Tuple = Queue{NTuple{6, Float64}}
+State_Vec = NTuple{6, Float64}
+Queue_State_Vec = Queue{State_Vec}
+
 ω = 0.1 # yaw_rate
 τ = 0.1 # time_step
 
@@ -12,41 +17,94 @@ Queue_Tuple = Queue{NTuple{6, Float64}}
 
 mutable struct Car
 
-	drive_path::Queue_Tuple
-	Car(drive_path::Queue_Tuple) = new(drive_path)
+	drive_path::Queue_State_Vec
+	no_traffic::Bool # @traffic is empty
+	traffic::Array{Car, 1}
+	Car(drive_path::Queue_State_Vec) = new(drive_path, true)
 
 end
 
 function get_drive_path(car::Car)
+
 	if(isempty(car.drive_path))
 	   return
-   end
+	end
+
 	return dequeue!(car.drive_path)
+
 end
 
-function yaw_rate(deg)
-	return deg*1.1
-end
+function predict_trajectory(car::Car, sec_ahead::Int64)
 
-function predict_trajectory(car::Car, sec_ahead)
-
-	new_q = Queue{NTuple{6, Float64}}()
-	enqueue!(new_q, first(car.drive_path))
+	q_n = Queue{State_Vec}()
+	enqueue!(q_n, first(car.drive_path))
 
 	for i = 0:τ:sec_ahead
-		(p_x, p_y, tmp1, tmp2, θ, vel) = last(new_q)
+		(p_x, p_y, tmp1, tmp2, θ, vel) = last(q_n)
 		p_x_n = p_x + vel/ω * (sin(θ + ω*τ) - sin(θ))
 		p_y_n = p_y + vel/ω * (cos(ω) - cos(ω+θ*τ))
 		θ_n= ω*τ+θ
-		enqueue!(new_q, (p_x_n, p_y_n, tmp1, tmp2, θ_n, vel))
+		enqueue!(q_n, (p_x_n, p_y_n, tmp1, tmp2, θ_n, vel))
 		println("PREDICTION: ", p_x_n, " (p_x_n), ", p_y_n, " (p_y_n), ", θ_n, " (new_angle), ", vel, " vel ") 
 	end
 
-	car.drive_path = new_q	
+	return q_n
+
+end
+
+function observe_new_car(car::Car, obs_car::Car)
+	if car.no_traffic == true
+		car.traffic = [obs_car]
+		car.no_traffic = false
+	else
+		append!(car.traffic, obs_car)
+	end
+
+	@spawn check_traffic(car)
+
+end
+
+function check_traffic(car::Car)
+
+	for x = 1:30
+		for i = 1:length(car.traffic)
+			signal = check_car(car, car.traffic[i])
+			if signal == -1
+				return
+			end
+		end
+		sleep(0.5)
+	end
+	
+	#check_traffic(car) -> should eventually become recursive
+	
+end
+
+function check_car(car::Car, obs_car::Car)
+
+	radius = 150 # attention-radius
+	(my_pos_x, my_pos_y) = first(car.drive_path)
+	(obs_pos_x, obs_pos_y) = first(obs_car.drive_path)
+	if (obs_pos_x - my_pos_x)^2 + (obs_pos_y - my_pos_y)^2 <= radius^2 # then it is inside of circle
+		predicted_traj = predict_trajectory(obs_car, 5)
+		return analyze_trajectory((my_pos_x, my_pos_y), deepcopy(predicted_traj))
+	end
 
 end
 
 
+function analyze_trajectory((pos_x, pos_y)::Tuple{Float64, Float64}, q::Queue_State_Vec)
 
+	radius = 100 # danger-radius
+	while isempty(q) == false
+		(obs_pos_x, obs_pos_y) = dequeue!(q)
+		if (obs_pos_x - pos_x)^2 + (obs_pos_y - pos_y)^2 <= radius^2 # then it is inside of circle
+			println("DANGER!!!!!!!!!!!!!!")
+			return -1 # danger-signal
+		end
+	end
 
+end
+
+end
 
--- street.jl
+++ street.jl
@@ -1,8 +1,9 @@
 import Base.Threads.@spawn
+include("car.jl")
+#using Main.CarModule
 using Makie
 using DataStructures
 
-include("car.jl")
 
 time = 0
 
@@ -18,9 +19,11 @@ lines!(scene, street_km, lineA, color = :green, linewidth = 4)
 lines!(scene, street_km, lineB, color = :red, linewidth = 4)
 lines!(scene, street_km, lineC, color = :green, linewidth = 4)
 
-function calc_normal_drive(p_x, p_y, a_x, a_y, θ, v, q)
+function calc_normal_drive(p_x::Float64, p_y::Float64, a_x::Float64, a_y::Float64, θ::Float64, v::Float64, q::CarModule.Queue_State_Vec)
+
 	x_add = v/10
-	for i = 1:0.1:8
+
+	for i = 1:0.1:13
 		yaw_angle = 0.0
 		if i >= 5
 			yaw_angle = 0.5
@@ -28,9 +31,10 @@ function calc_normal_drive(p_x, p_y, a_x, a_y, θ, v, q)
 		enqueue!(q, (p_x+x_add, p_y, a_x, a_y, θ, v))
 		x_add = x_add + v/10
 	end
+
 end
 
-function paint_car(obs_car)
+function paint_car(obs_car::Array{Observable{Float64},1})
 
 	obs1 = lift(x -> [x], obs_car[1])
 	obs2 = lift(x -> [x], obs_car[2])
@@ -40,13 +44,14 @@ function paint_car(obs_car)
 
 end
 
-function drive_car(car::Car, obs_car)
+function drive_car(car::CarModule.Car, obs_car::Array{Observable{Float64},1}, cause_accident::Bool)
 
 	for i = 1:0.1:10
-		if i == 5 
-			predict_trajectory(car, 3)
+		if i == 5 && cause_accident == true
+			q = CarModule.predict_trajectory(car, 6)
+			car.drive_path = q
 		end
-		position_vector = get_drive_path(car)
+		position_vector = CarModule.get_drive_path(car)
 		println(position_vector)
 		obs_car[1][] = position_vector[1]
 		obs_car[2][] = position_vector[2]
@@ -58,23 +63,26 @@ function drive_car(car::Car, obs_car)
 end
 
 # build car_1
-q_drivepath_c1 = Queue{NTuple{6, Float64}}()
-calc_normal_drive(25.0, 100.0, 30.0, 0.0, 0.5, 50, q_drivepath_c1)
+q_drivepath_c1 = Queue{CarModule.State_Vec}()
+calc_normal_drive(25.0, 100.0, 30.0, 0.0, 0.5, 50.0, q_drivepath_c1)
 obs_car1 = [Node(25.0), Node(100.0), Node(30.0), Node(0.0)]
-car_1 = Car(q_drivepath_c1)
+car_1 = CarModule.Car(q_drivepath_c1)
 paint_car(obs_car1)
 
 # build car_2
-q_drivepath_c2 = Queue{NTuple{6, Float64}}()
-calc_normal_drive(25.0, 400.0, 30.0, 0.0, 0.001, 30, q_drivepath_c2)
+q_drivepath_c2 = Queue{CarModule.State_Vec}()
+calc_normal_drive(25.0, 400.0, 30.0, 0.0, 0.0, 40.0, q_drivepath_c2)
 obs_car2 = [Node(500.0), Node(400.0), Node(-30.0), Node(0.0)]
-car_2 = Car(q_drivepath_c2)
+car_2 = CarModule.Car(q_drivepath_c2)
 paint_car(obs_car2)
 
 display(scene)
-@spawn drive_car(car_1, obs_car1)
-#drive_car(car_1, obs_car1)
-drive_car(car_2, obs_car2)
+
+@spawn CarModule.observe_new_car(car_1, car_2)
+CarModule.observe_new_car(car_2, car_1)
+
+@spawn drive_car(car_1, obs_car1, true)
+drive_car(car_2, obs_car2, false)
 
 
 
